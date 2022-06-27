5f624afe6382bc1764c4e10aeb7315ace2ed32c7
Fariza Kubanova <farizatkubanova@gmail.com>
2022-06-27 20:40:56 +0100

    переменные внутри функций

--- validator_static/index.js
+++ validator_static/index.js
@@ -1,29 +1,33 @@
 class Validator {
-    email = document.querySelector('#email').value;
+    
     static isEmail(email) {
+        email = document.querySelector('#email').value;
         let isEmail = false;
-        if (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email.value))
+        if (/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email))
             isEmail = true;
         return isEmail;
     }
-    domain = document.querySelector('#domain').value;
+    
     static isDomain(domain) {
+        domain = document.querySelector('#domain').value;
         let isDomain = false;
-        if (/^((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/.test(domain.value))
+        if (/^((?!-))(xn--)?[a-z0-9][a-z0-9-_]{0,61}[a-z0-9]{0,1}\.(xn--)?([a-z0-9\-]{1,61}|[a-z0-9-]{1,30}\.[a-z]{2,})$/.test(domain))
             isDomain = true;
         return isDomain;
     }
-    date = document.querySelector('#date').value;
+    
     static isDate(date) {
         let isDate = false;
-        if (/^(([1-9]|1[012])[-/.]([1-9]|[12][0-9]|3[01])[-/.](19|20)\d\d)|((1[012]|0[1-9])(3[01]|2\d|1\d|0[1-9])(19|20)\d\d)|((1[012]|0[1-9])[-/.](3[01]|2\d|1\d|0[1-9])[-/.](19|20)\d\d)$/.test(date.value))
+        date = document.querySelector('#date').value;
+        if (/^(([1-9]|1[012])[-/.]([1-9]|[12][0-9]|3[01])[-/.](19|20)\d\d)|((1[012]|0[1-9])(3[01]|2\d|1\d|0[1-9])(19|20)\d\d)|((1[012]|0[1-9])[-/.](3[01]|2\d|1\d|0[1-9])[-/.](19|20)\d\d)$/.test(date))
             isDate = true;
         return isDate;
     }
-    phone = document.querySelector('#phone').value;
+    
     static isPhone(phone) {
         let isPhone = false;
-        if (/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/.test(phone.value))
+        phone = document.querySelector('#phone').value;
+        if (/^(\s*)?(\+)?([- _():=+]?\d[- _():=+]?){10,14}(\s*)?$/.test(phone))
             isPhone = true;
         return isPhone;
     }
